c4abbd9f1b6f42190fe6cbc1951d263f9639a13e
Kamvacodes2 <mtikasoga96@gmail.com>
2024-09-03 12:08:21 +0200

    Create 15.19-15.20.sql

---
+++ Chapter 15/15.19-15.20.sql
@@ -0,0 +1,33 @@
+-- 15-19: Creating the grades_update trigger
+
+CREATE TRIGGER grades_update
+  AFTER UPDATE
+  ON grades
+  FOR EACH ROW
+  EXECUTE PROCEDURE record_if_grade_changed();
+
+--  15-20: Testing the grades_update trigger
+
+-- Initially, there should be 0 records in the history
+SELECT * FROM grades_history;
+
+
+SELECT * FROM grades;
+
+-- before id 1 grade = F 
+-- Update a grade
+UPDATE grades
+SET grade = 'C'
+WHERE student_id = 1 AND course_id = 1;
+
+SELECT * FROM grades;
+
+-- Now check the history
+SELECT student_id,
+       change_time,
+       course,
+       old_grade,
+       new_grade
+FROM grades_history;
+
+
